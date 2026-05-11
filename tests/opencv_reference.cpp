349eb4040693b54cd581b05062d72c2d039d1307
Горбузова Мария <gorbuzova.maria@yandex.ru>
2026-05-12 00:48:28 +0300

    test: use stb for reference image input/output to reduce read/write differences

--- tests/opencv_reference.cpp
+++ tests/opencv_reference.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
+#define STB_IMAGE_IMPLEMENTATION
+#include "../src/stb_image.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "../src/stb_image_write.h"
 #include <opencv2/opencv.hpp>
 
 int main(int argc, char **argv) {
@@ -7,14 +11,14 @@ int main(int argc, char **argv) {
         return -1;
     }
 
-    cv::Mat img_color = cv::imread(argv[1], cv::IMREAD_COLOR);
-    if (img_color.empty()) {
+    int width, height, channels;
+    unsigned char *img_data = stbi_load(argv[1], &width, &height, &channels, 3);
+    if (!img_data) {
         std::cout << "Could not read image: " << argv[1] << std::endl;
         return -1;
     }
-
-    cv::Mat img;
-    cv::cvtColor(img_color, img, cv::COLOR_BGR2RGB);
+    cv::Mat img(height, width, CV_8UC3, img_data);
+    channels = 3;
 
     cv::Mat kernel_blur = cv::Mat::ones(3, 3, CV_32F) / 9.0f;
     cv::Mat kernel_identity = cv::Mat::zeros(3, 3, CV_32F);
@@ -28,15 +32,29 @@ int main(int argc, char **argv) {
     cv::Mat kernel_sobel_x_flipped;
     cv::flip(kernel_sobel_x, kernel_sobel_x_flipped, -1);
     cv::filter2D(img, sobel_result, CV_32F, kernel_sobel_x_flipped);
-
     cv::Mat blur_show, identity_show, sobel_show;
     blur_result.convertTo(blur_show, CV_8U);
     identity_result.convertTo(identity_show, CV_8U);
     cv::convertScaleAbs(sobel_result, sobel_show);
 
-    cv::imwrite("blur_result.png", blur_show);
-    cv::imwrite("identity_result.png", identity_show);
-    cv::imwrite("sobel_result.png", sobel_show);
+    stbi_write_png("blur_result.png",
+                   blur_show.cols,
+                   blur_show.rows,
+                   blur_show.channels(),
+                   blur_show.data,
+                   blur_show.step);
+    stbi_write_png("identity_result.png",
+                   identity_show.cols,
+                   identity_show.rows,
+                   identity_show.channels(),
+                   identity_show.data,
+                   identity_show.step);
+    stbi_write_png("sobel_result.png",
+                   sobel_show.cols,
+                   sobel_show.rows,
+                   sobel_show.channels(),
+                   sobel_show.data,
+                   sobel_show.step);
 
     std::cout << "Results saved:\n";
     std::cout << "  Box blur: blur_result.png\n";
