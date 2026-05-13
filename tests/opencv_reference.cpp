5c7d4c3c074f7cb50395c74d09a6e9202e45ae99
Горбузова Мария <gorbuzova.maria@yandex.ru>
2026-05-13 03:32:02 +0300

    test: add openCV border mode handling for convolution tests

--- tests/opencv_reference.cpp
+++ tests/opencv_reference.cpp
@@ -1,16 +1,32 @@
 #include <iostream>
+#include <cstring>
 #define STB_IMAGE_IMPLEMENTATION
 #include "../src/stb_image.h"
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "../src/stb_image_write.h"
 #include <opencv2/opencv.hpp>
 
+
 int main(int argc, char **argv) {
-    if (argc != 2) {
-        std::cout << "Usage: " << argv[0] << " <image_path>" << std::endl;
+    if (argc < 2 || argc > 3) {
+        std::cout << "Usage: " << argv[0] << " <image_path> [border_mode]" << std::endl;
         return -1;
     }
-
+    const char *border_mode_name = "reflect101";
+    if (argc == 3) {
+        border_mode_name = argv[2];
+    }
+    int border_type = cv::BORDER_REFLECT_101;
+    if (std::strcmp(border_mode_name, "reflect") == 0) {
+        border_type = cv::BORDER_REFLECT;
+    } else if (std::strcmp(border_mode_name, "replicate") == 0) {
+        border_type = cv::BORDER_REPLICATE;
+    } else if (std::strcmp(border_mode_name, "constant") == 0) {
+        border_type = cv::BORDER_CONSTANT;
+    } else if (std::strcmp(border_mode_name, "reflect101") != 0) {
+        std::cout << "Unknown border mode: " << border_mode_name << std::endl;
+        return -1;
+    }
     int width, height, channels;
     unsigned char *img_data = stbi_load(argv[1], &width, &height, &channels, 3);
     if (!img_data) {
@@ -27,11 +43,11 @@ int main(int argc, char **argv) {
         (cv::Mat_<float>(3, 3) << -1, 0, 1, -2, 0, 2, -1, 0, 1);
 
     cv::Mat blur_result, identity_result, sobel_result;
-    cv::filter2D(img, blur_result, CV_32F, kernel_blur);
-    cv::filter2D(img, identity_result, CV_32F, kernel_identity);
+    cv::filter2D(img, blur_result, CV_32F, kernel_blur, cv::Point(-1, -1), 0, border_type);
+    cv::filter2D(img, identity_result, CV_32F, kernel_identity, cv::Point(-1, -1), 0, border_type);
     cv::Mat kernel_sobel_x_flipped;
     cv::flip(kernel_sobel_x, kernel_sobel_x_flipped, -1);
-    cv::filter2D(img, sobel_result, CV_32F, kernel_sobel_x_flipped);
+    cv::filter2D(img, sobel_result, CV_32F, kernel_sobel_x_flipped, cv::Point(-1, -1), 0, border_type);
     cv::Mat blur_show, identity_show, sobel_show;
     blur_result.convertTo(blur_show, CV_8U);
     identity_result.convertTo(identity_show, CV_8U);
